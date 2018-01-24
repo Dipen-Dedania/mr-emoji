5f8b424049e6355d09ddbce2d64278fe0964c4da
Dipen <ddipen>
2018-01-24 19:50:35 +0530

    Exported parser component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import emojiIndex from './utils/emoji-index'
 import store from './utils/store'
 import frequently from './utils/frequently'
 
-export { Picker, Emoji, Category } from './components'
+export { Picker, Emoji, Category, Parser } from './components'
 export { emojiIndex, store, frequently }
